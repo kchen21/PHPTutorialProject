d4e9a539a49a508e84b913a20b77e5ca8d65ec96
Kenneth Chen <kennethchendbgt@gmail.com>
2017-08-24 17:08:10 -0400

    Create and test Page controller

---
+++ src/Controllers/Page.php
@@ -0,0 +1,11 @@
+<?php
+  declare(strict_types = 1);
+
+  namespace PHPTutorialProject\Controllers;
+
+  class Page {
+    public function show($params) {
+      var_dump($params);
+    }
+  }
+?>
--- src/Routes.php
+++ src/Routes.php
@@ -2,6 +2,7 @@
   declare(strict_types = 1);
 
   return [
-    ['GET', '/', ['PHPTutorialProject\Controllers\Homepage', 'show']]
+    ['GET', '/', ['PHPTutorialProject\Controllers\Homepage', 'show']],
+    ['GET', '/{slug}', ['PHPTutorialProject\Controllers\Page', 'show']] // $params will receive the slug of the page
   ];
 ?>
