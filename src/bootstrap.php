e0c75c4112b4cb29e07fbb74005d39a257130430
Kenneth Chen <kennethchendbgt@gmail.com>
2017-08-23 22:10:10 -0400

    Install and test FastRoute

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -25,11 +25,33 @@
   $request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
   $response = new \Http\HttpResponse; // stores response data
 
-  // $content = '<h1>Hello World</h1>';
-  // $response->setContent($content);
+  // Register the application's available routes
+  $dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/hello-world', function () {
+      echo 'Hello World';
+    });
+    $r->addRoute('GET', '/another-route', function () {
+      echo 'This works too';
+    });
+  });
 
-  $response->setContent('404 - Page not found');
-  $response->setStatusCode(404);
+  // Call the dispatcher and execute the appropriate part of the switch statement
+  $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+  switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+      $response->setContent('404 - Page not found');
+      $response->setStatusCode(404);
+      break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+      $response->setContent('405 - Method not allowed');
+      $response->setStatusCode(405);
+      break;
+    case \FastRoute\Dispatcher::FOUND:
+      $handler = $routeInfo[1];
+      $vars = $routeInfo[2];
+      call_user_func($handler, $vars);
+      break;
+  }
 
   // required in order to send the response data to the browser
   foreach ($response->getHeaders() as $header) {
